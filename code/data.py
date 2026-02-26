8300171dac0b15b6fa1773f969fb6dbad2bce3ff
ADAM <167138284+Brokttv@users.noreply.github.com>
2026-02-25 22:39:21 -0500

    Update data.py

--- code/data.py
+++ code/data.py
@@ -1,6 +1,7 @@
 
-def data(seq_len:int, num_samples:int,Majority:bool):
 
+def data(seq_len:int, num_samples:int,Majority:bool):
+  
   if Majority:
     X = np.random.randint(0,2,(num_samples,seq_len))
     y = (X.sum(axis=1) > seq_len //2).astype(int)
@@ -8,5 +9,6 @@ def data(seq_len:int, num_samples:int,Majority:bool):
     X = np.zeros((num_samples,seq_len))
     X[0::2,:] = np.random.randint(0,2,(num_samples//2, seq_len))
     y = (X.sum(axis=1)>0).astype(int)
-
+  
   return X,y
+  
